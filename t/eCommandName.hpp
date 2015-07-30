3062a9bc62aa9412dafcc58c020c71b675bd2c2b
Frederic <fcosnefr@Frederics-Mac-Pro.local>
2015-07-30 09:55:17 -0700

    clean 5

--- t/eCommandName.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef ECOMMANDNAME_HPP
-#define ECOMMANDNAME_HPP
-
-enum eCommandName { Unknown = -1, Push, Assert, Pop, Add, Sub, Mul, Div, Mod, Print, Dump, Exit };
-
-#endif
--- t/eOperandType.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef EOPERANDTYPE
-#define EOPERANDTYPE
-
-enum eOperandType { Void = -1, Int8, Int16, Int32, Float, Double };
-
-#endif
